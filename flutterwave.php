459b3f5975a1cf4b102ce29dbc04b91e1c83c175
Olaobaju Abraham <olaobajua@gmail.com>
2021-12-17 09:05:45 +0100

    update: added wordpress readme

--- flutterwave.php
+++ flutterwave.php
@@ -15,21 +15,22 @@
  * Copyright: © 2021 Flutterwave
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly
 }
 
-define( 'WC_FLUTTERWAVE_VERSION', '1.1.0' );
-define( 'WC_FLUTTERWAVE_PLUGIN_FILE', __FILE__ );
-define( 'WC_FLUTTERWAVE_DIR_PATH', plugin_dir_path( WC_FLUTTERWAVE_PLUGIN_FILE ) );
-define( 'WC_FLUTTERWAVE_URL', trailingslashit( plugins_url( '/', __FILE__ ) ) );
+define('WC_FLUTTERWAVE_VERSION', '1.1.0');
+define('WC_FLUTTERWAVE_PLUGIN_FILE', __FILE__);
+define('WC_FLUTTERWAVE_DIR_PATH', plugin_dir_path(WC_FLUTTERWAVE_PLUGIN_FILE));
+define('WC_FLUTTERWAVE_URL', trailingslashit(plugins_url('/', __FILE__)));
 /**
  * The code that runs during plugin activation.
  * This action is documented in includes/class-flutterwave-activator.php
  */
-function activate_wc_flutterwave() {
+function activate_wc_flutterwave()
+{
 	// Test to see if WooCommerce is active (including network activated).
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-flutterwave-activator.php';
+	require_once plugin_dir_path(__FILE__) . 'includes/class-flutterwave-activator.php';
 	Flutterwave_Activator::activate();
 }
 
@@ -37,20 +38,21 @@ function activate_wc_flutterwave() {
  * The code that runs during plugin deactivation.
  * This action is documented in includes/class-flutterwave-deactivator.php
  */
-function deactivate_wc_flutterwave() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-flutterwave-deactivator.php';
+function deactivate_wc_flutterwave()
+{
+	require_once plugin_dir_path(__FILE__) . 'includes/class-flutterwave-deactivator.php';
 	Flutterwave_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_wc_flutterwave' );
-register_deactivation_hook( __FILE__, 'deactivate_wc_flutterwave' );
+register_activation_hook(__FILE__, 'activate_wc_flutterwave');
+register_deactivation_hook(__FILE__, 'deactivate_wc_flutterwave');
 
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-flutterwave.php';
+require plugin_dir_path(__FILE__) . 'includes/class-flutterwave.php';
 
 
 /**
@@ -62,21 +64,23 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-flutterwave.php';
  *
  * @since    1.1.0
  */
-function run_wc_flutterwave() {
+function run_wc_flutterwave()
+{
 
 	$plugin = new Flutterwave();
 	$plugin->run();
-
 }
 
-function flw_add_action_plugin( $links ) 
+function flw_add_action_plugin($links)
 {
-	$flonboarding = esc_url( get_admin_url( null, 'admin.php?page=wc-settings&tab=checkout' ) );
-	$mylinks_flw = array('<a href="'.$flonboarding.'">' . __('Settings', 'General') . '</a>',
-      '<a href="https://developer.flutterwave.com/discuss" target="_blank">Support</a>');
-    return array_merge( $links, $mylinks_flw );
+	$flonboarding = esc_url(get_admin_url(null, 'admin.php?page=wc-settings&tab=checkout'));
+	$mylinks_flw = array(
+		'<a href="' . $flonboarding . '">' . __('Settings', 'General') . '</a>',
+		'<a href="https://developer.flutterwave.com/discuss" target="_blank">Support</a>'
+	);
+	return array_merge($links, $mylinks_flw);
 }
-add_filter( 'plugin_action_links_'.plugin_basename(__FILE__),'flw_add_action_plugin');
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'flw_add_action_plugin');
 
 
 /**
@@ -84,54 +88,59 @@ add_filter( 'plugin_action_links_'.plugin_basename(__FILE__),'flw_add_action_plu
  *
  * @since 4.1.2
  */
-function woocommerce_flutterwave_missing_wc_notice() {
+function woocommerce_flutterwave_missing_wc_notice()
+{
 	/* translators: 1. URL link. */
-	echo '<div class="error"><p><strong>' . sprintf( esc_html__( 'Flutterwave requires WooCommerce to be installed and active. You can download %s here.', 'flutterwave' ), '<a href="https://woocommerce.com/" target="_blank">WooCommerce</a>' ) . '</strong></p></div>';
+	echo '<div class="error"><p><strong>' . sprintf(esc_html__('Flutterwave requires WooCommerce to be installed and active. You can download %s here.', 'flutterwave'), '<a href="https://woocommerce.com/" target="_blank">WooCommerce</a>') . '</strong></p></div>';
 }
 
-function init_flutterwave_woocommerce() {
-	if ( ! class_exists( 'WooCommerce' ) ) {
-		add_action( 'admin_notices', 'woocommerce_flutterwave_missing_wc_notice' );
+function init_flutterwave_woocommerce()
+{
+	if (!class_exists('WooCommerce')) {
+		add_action('admin_notices', 'woocommerce_flutterwave_missing_wc_notice');
 		return;
 	}
 
-	include_once( 'includes/class-flutterwave-gateway.php' );
+	include_once('includes/class-flutterwave-gateway.php');
 
 	new WP_Flutterwave_Settings_Rest_Route(WC_Flutterwave_Gateway::getInstance());
 	new WP_Flutterwave_Subaccounts_Rest_Route(WC_Flutterwave_Gateway::getInstance());
 	new WP_Flutterwave_Transactions_Rest_Route(WC_Flutterwave_Gateway::getInstance());
 	new WP_Flutterwave_Plan_Rest_Route(WC_Flutterwave_Gateway::getInstance());
-	add_filter( 'woocommerce_payment_gateways', 'add_flutterwave_class' );
+	add_filter('woocommerce_payment_gateways', 'add_flutterwave_class');
 }
-add_action( 'plugins_loaded', 'init_flutterwave_woocommerce', 0 );
+add_action('plugins_loaded', 'init_flutterwave_woocommerce', 0);
 
 function load_scripts($hook)
 {
 	$flw_woo_check = false;
-	if ( class_exists( 'woocommerce' ) ) { $flw_woo_check = true;}
+	if (class_exists('woocommerce')) {
+		$flw_woo_check = true;
+	}
 
-	if($flw_woo_check){
-		wp_enqueue_script('flutterwavenew_js', WC_FLUTTERWAVE_URL. 'admin/sample/test.js');
-		wp_localize_script( 'flutterwavenew_js', 'flutterwave_data', [
-			'apiUrl' => home_url( '/wp-json' ),
-			'nonce' => wp_create_nonce( 'wp_rest' ),
+	if ($flw_woo_check) {
+		wp_enqueue_script('flutterwavenew_js', WC_FLUTTERWAVE_URL . 'admin/sample/test.js');
+		wp_localize_script('flutterwavenew_js', 'flutterwave_data', [
+			'apiUrl' => home_url('/wp-json'),
+			'nonce' => wp_create_nonce('wp_rest'),
 			'hook' => $hook,
 			'logo_src' => plugins_url('src/icons/flutterwave-logo.svg', __FILE__),
 			'webhook' => WC()->api_request_url('Flutterwave_WC_Payment_Webhook')
 		]);
 	}
 }
-add_action('admin_enqueue_scripts','load_scripts');
+add_action('admin_enqueue_scripts', 'load_scripts');
 
-add_action( 'woocommerce_blocks_loaded', 'woocommerce_flutterwave_woocommerce_blocks_support' );
+add_action('woocommerce_blocks_loaded', 'woocommerce_flutterwave_woocommerce_blocks_support');
 
-function woocommerce_flutterwave_woocommerce_blocks_support() {
-	if ( class_exists( 'Automattic\WooCommerce\Blocks\Payments\Integrations\AbstractPaymentMethodType' ) ) {
-		require_once dirname( __FILE__ ) . '/includes/class-wc-gateway-flutterwave-blocks-support.php';
+function woocommerce_flutterwave_woocommerce_blocks_support()
+{
+	if (class_exists('Automattic\WooCommerce\Blocks\Payments\Integrations\AbstractPaymentMethodType')) {
+		require_once dirname(__FILE__) . '/includes/class-wc-gateway-flutterwave-blocks-support.php';
 		add_action(
 			'woocommerce_blocks_payment_method_type_registration',
-			function( Automattic\WooCommerce\Blocks\Payments\PaymentMethodRegistry $payment_method_registry ) {
-				$payment_method_registry->register( new WC_Gateway_Flutterwave_Blocks_Support );
+			function (Automattic\WooCommerce\Blocks\Payments\PaymentMethodRegistry $payment_method_registry) {
+				$payment_method_registry->register(new WC_Gateway_Flutterwave_Blocks_Support);
 			}
 		);
 	}
--- readme.txt
+++ readme.txt
@@ -1,62 +1,66 @@
 === Flutterwave ===
-Contributors: (this should be a list of wordpress.org userid's)
-Donate link: https://example.com/
-Tags: comments, spam
+Contributors: flutterwave
+Tags: payment, flutterwave, international payments, PayPal, Endless possibilities
 Requires at least: 4.5
-Tested up to: 5.9-alpha-51818
+Tested up to: 5.8
 Requires PHP: 5.6
-Stable tag: 0.1.0
+Stable tag: 1.1.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
-Here is a short description of the plugin.  This should be no more than 150 characters.  No markup here.
+Accept Credit card, Debit card and Bank account payment directly on your store with the official Flutterwave for Woo commerce.
+
+== Getting Started ==
+
+= Minimum Requirements =
+
+* Latest release versions of WordPress and WooCommerce ([read more here](https://developer.woocommerce.com/?p=9998))
+* PHP version 7.0 or greater (PHP 7.4 or greater is recommended)
+* MySQL version 5.6 or greater
+
+Visit the [WooCommerce server requirements documentation](https://docs.woocommerce.com/document/server-requirements/) for a detailed list of server requirements.
 
 == Description ==
-
-This is the long description.  No limit, and you can use Markdown (as well as in the following sections).
-
-For backwards compatibility, if this section is missing, the full length of the short description will be used, and
-Markdown parsed.
-
-A few notes about the sections above:
-
-*   "Contributors" is a comma separated list of wp.org/wp-plugins.org usernames
-*   "Tags" is a comma separated list of tags that apply to the plugin
-*   "Requires at least" is the lowest version that the plugin will work on
-*   "Tested up to" is the highest version that you've *successfully used to test the plugin*. Note that it might work on
-higher versions... this is just the highest one you've verified.
-*   Stable tag should indicate the Subversion "tag" of the latest stable version, or "trunk," if you use `/trunk/` for
-stable.
-
-    Note that the `readme.txt` of the stable tag is the one that is considered the defining one for the plugin, so
-if the `/trunk/readme.txt` file says that the stable tag is `4.3`, then it is `/tags/4.3/readme.txt` that'll be used
-for displaying information about the plugin.  In this situation, the only thing considered from the trunk `readme.txt`
-is the stable tag pointer.  Thus, if you develop in trunk, you can update the trunk `readme.txt` to reflect changes in
-your in-development version, without having that information incorrectly disclosed about the current stable version
-that lacks those changes -- as long as the trunk's `readme.txt` points to the correct stable tag.
-
-    If no stable tag is provided, it is assumed that trunk is stable, but you should specify "trunk" if that's where
-you put the stable version, in order to eliminate any doubt.
+Accept payment via MasterCard, Visa, Mpesa, PayPal, TIGO, Mobile money, Bank accounts and Barter.
 
 == Installation ==
 
-This section describes how to install the plugin and get it working.
+= Automatic Installation =
+*   Login to your WordPress Dashboard.
+*   Click on "Plugins > Add New" from the left menu.
+*   In the search box type __flutterwave__.
+*   Click on __Install Now__ on __fluterwave__ to install the plugin on your site.
+*   Confirm the installation.
+*   Activate the plugin.
+*   Click on "WooCommerce > Settings" from the left menu and click the "Checkout" tab.
+*   Click on the __Flutterwave for Woocommerce__ link from the available Checkout Options
+*   Configure your __Flutterwave for Woocommerce__ settings accordingly.
 
-e.g.
+= Manual Installation =
+1.  Download the plugin zip file.
+2.  Login to your WordPress Admin. Click on "Plugins > Add New" from the left menu.
+3.  Click on the "Upload" option, then click "Choose File" to select the zip file you downloaded. Click "OK" and "Install Now" to complete the installation.
+4.  Activate the plugin.
+5.  Click on "WooCommerce > Settings" from the left menu and click the "Checkout" tab.
+6.  Click on the __Flutterwave for Woocommerce__ link from the available Checkout Options
+7. Configure your __Flutterwave for Woocommerce__ settings accordingly.
 
-1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
-1. Activate the plugin through the 'Plugins' menu in WordPress
-1. Place `<?php do_action('plugin_name_hook'); ?>` in your templates
+For FTP manual installation, [check here](http://codex.wordpress.org/Managing_Plugins#Manual_Plugin_Installation).
+
 
 == Frequently Asked Questions ==
 
-= A question that someone might have =
+= Does this version support woocommerce blocks =
 
-An answer to that question.
+As a matter of fact. this version does. 
 
-= What about foo bar? =
+= Any plans for continuous wordpress block development =
 
-Answer to foo bar dilemma.
+yes we are looking into the customization of all payment pages and widgets via the block editor.
+
+= Where can I report bugs or contribute to the project? =
+
+Bugs should be reported in the [Flutterwave WooCommerce repository](https://github.com/Flutterwave/Flutterwave-WooCommerce).
 
 == Screenshots ==
 
@@ -68,48 +72,25 @@ directory take precedence. For example, `/assets/screenshot-1.png` would win ove
 
 == Changelog ==
 
-= 1.0 =
-* A change since the previous version.
-* Another change.
+= 1.1.0 - 2021-09-15 =
 
-= 0.5 =
-* List versions from most recent at top to oldest at bottom.
+- Fix conflict with WooCommerce Admin.
 
-== Upgrade Notice ==
+= 1.0.0 - 2021-08-08 =
 
-= 1.0 =
-Upgrade notices describe the reason a user should upgrade.  No more than 300 characters.
+- Fix: log now using WC-logger.
+- Update: Added new Payment Methods.
 
-= 0.5 =
-This version fixes a security related bug.  Upgrade immediately.
+= 0.1.1 - 2021-06-14 =
 
-== Arbitrary section ==
+- Fix: Fixed the webhook url displayed.
+- Update: Added Support for Woocommerce Blocks.
 
-You may provide arbitrary sections, in the same format as the ones above.  This may be of use for extremely complicated
-plugins where more information needs to be conveyed that doesn't fit into the categories of "description" or
-"installation."  Arbitrary sections will be shown below the built-in sections outlined above.
+= 0.1.0 - 2021-04-22 =
 
-== A brief Markdown Example ==
+- Update: Updated Admin Frontend.
+- Fix: Fixed Conflict with WooCommerce.
 
-Ordered list:
 
-1. Some feature
-1. Another feature
-1. Something else about the plugin
 
-Unordered list:
 
-* something
-* something else
-* third thing
-
-Here's a link to [WordPress](https://wordpress.org/ "Your favorite software") and one to [Markdown's Syntax Documentation][markdown syntax].
-Titles are optional, naturally.
-
-[markdown syntax]: https://daringfireball.net/projects/markdown/syntax
-            "Markdown is what the parser uses to process much of the readme file"
-
-Markdown uses email style notation for blockquotes and I've been told:
-> Asterisks for *emphasis*. Double it up  for **strong**.
-
-`<?php code(); // goes in backticks ?>`
