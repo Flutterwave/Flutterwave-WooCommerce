6c5b25190f6a313d4b64c57c4d98e419a42f24c6
Olaobaju Abraham <olaobajua@gmail.com>
2022-01-25 21:23:04 +0100

    update: refactored flutterwave-admin.php

--- admin/class-flutterwave-admin.php
+++ admin/class-flutterwave-admin.php
@@ -176,6 +176,23 @@ class Flutterwave_Admin {
         // );
     }
 
+	/**
+	 * Santizes an array's values
+	 */
+	public function flw_sanitize( $input ) {
+
+		// Initialize the new array that will hold the sanitize values
+		$new_input = array();
+
+		// Loop through the input and sanitize each of the values
+		foreach ( $input as $key => $val ) {
+			$new_input[ $key ] = sanitize_text_field( $val );
+		}
+
+		return $new_input;
+
+	}
+
 
 	public function maybe_redirect_to_onboarding()
 	{
@@ -346,19 +363,7 @@ class Flutterwave_Admin {
 
 	}
 
-	public function flw_sanitize( $input ) {
 
-			// Initialize the new array that will hold the sanitize values
-			$new_input = array();
-
-			// Loop through the input and sanitize each of the values
-			foreach ( $input as $key => $val ) {
-				$new_input[ $key ] = sanitize_text_field( $val );
-			}
-
-			return $new_input;
-
-	}
 
 	/**
     * settings link
